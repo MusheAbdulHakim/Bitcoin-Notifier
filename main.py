2e91ff0e9517df37f21c52b169b7a219545cc330
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-05-12 22:24:35 +0000

    optionally pass preferred crypto cli

--- main.py
+++ main.py
@@ -1,47 +1,59 @@
-from notifypy import Notify 
+from notifypy import Notify
 import time, schedule,optparse
 from pycoingecko import CoinGeckoAPI
 
 
 class App():
-    def __init__(self):
+    def __init__(self,crypto_coin):
         self.coingecko = CoinGeckoAPI()
         self.notification = Notify()
-        self.bitcoin = self.coingecko.get_price(ids='bitcoin', vs_currencies='usd')
+        self.crypto_coin = crypto_coin  
+        self.currency = 'usd'
+        self.crypto = self.coingecko.get_price(ids=self.crypto_coin, vs_currencies=self.currency)
         self.title = "Bitcoin Current Price"
-        self.message = "The current price of bitcoin in usd is: $"+ str(self.Price(self.bitcoin))
-        self.notification.icon = "./icons/bitcoin.png"
-        self.notification.audio = "./sounds/notification.wav"
+        self.message = "The current price of "+ self.crypto_coin + " is $"+str(self.Price())
+        self.notification.icon = icon
+        self.notification.audio = sound
 
     def notifyMe(self):
         self.notification.title = self.title
         self.notification.message = self.message
         self.notification.send()
 
-    def Price(self,coin):
+    def Price(self):
         coin_price = 0
-        for price in coin:
-            coin_price = coin[price]
-        return coin_price['usd']
+        for price in self.crypto:
+            coin_price = self.crypto[price]
+        return coin_price[self.currency]
 
-#instantiate our app class
-app = App()
+
+icon = "./icons/bitcoin.png"
+sound = "./sounds/notification.wav"
 parser = optparse.OptionParser()
 
+parser.add_option("-t","--time",dest="time",help="Time in (minutes) you want to be receiving notifications.")
+parser.add_option('-c',"--coin",dest="crypto",help="Crypto currency price you want.")
+(options,arguments) = parser.parse_args()
+notify_time = options.time
+
+CRYPTO_CURRENCY = 'bitcoin'
+
+if options.crypto:
+    CRYPTO_CURRENCY = options.crypto
+
+app = App(CRYPTO_CURRENCY.lower())
+
+
+def get_notification():
+    return app.notifyMe()
 
 try:
-    #returns the notification which is callable as required by schedule
-    def get_notification():
-        return app.notifyMe()
-
-    parser.add_option("-t","--time",dest="time",help="Time in (minutes) you want to be receiving notifications.")
-    (options,arguments) = parser.parse_args()
-    notify_time = int(options.time)
-
+    
     #call get_notification function to the schedule
     if notify_time:
-        schedule.every(notify_time).minutes.do(get_notification)
-    schedule.every(1).minutes.do(get_notification)
+        schedule.every(int(notify_time)).minutes.do(get_notification)
+    # schedule.every(1).minutes.do(get_notification)
+    schedule.every(10).seconds.do(get_notification)
     #loop for our schedules
     while True:
         schedule.run_pending()
