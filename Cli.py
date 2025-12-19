7200ddf355c35063a32f74c5cc56ac0cdc6e557a
musheabdulhakim <musheabdulhakim99@gmail.com>
2025-12-19 17:08:11 +0000

    code organized

--- Cli.py
+++ Cli.py
@@ -4,52 +4,53 @@ parser = argparse.ArgumentParser(description="Parse all commands parsed to the b
 
 
 class Cli:
-    def __init__(self):
-        pass
-
     def parse_options(self):
         parser.add_argument(
             '-t','--time',
-            type=int,default=1,
+            type=str,
+            default='1m',
             dest='time',
             help="Set timer for notification to show. Default[1 minute]"
         )
 
-        parser.add_argument(
-            '-T','--timeType',
-            type=str,default='m',
-            dest='timeType',
-            help="Set timer to use seconds or minutes.(s) for seconds or (m) for minutes Default [minutes]"
-        )
         
         parser.add_argument(
             '-c','--coin',
-            type=str,dest='coin',default='bitcoin',
+            type=str,
+            dest='coin',
+            default='bitcoin',
             help="Set crypto currency to use. Default [bitcoin]"
         )
 
         parser.add_argument(
             "-L",
             "--listcoins",
-            dest="listcoins",default=False,type=bool,
+            dest="listcoins",
+            default=False,
+            type=bool,
             help="Get all supported coins. Takes a boolearn (True or False)"
         )
 
         parser.add_argument(
-            '-C','--currency',dest='currency',
-            default='usd',type=str,
+            '-C','--currency',
+            dest='currency',
+            default='usd',
+            type=str,
             help="Currency you want to receive notification on.Default[usd]"
         )
 
         parser.add_argument(
             '-l',
             "--listcurrency",
-            dest="listcurrencies",default=False,type=bool,
-            help="Get list of supported curriencies.Takes a boolearn (True or False)"
+            dest="listcurrencies",
+            default=False,type=bool,
+            help="Get list of supported curriencies.Takes a boolean (True or False)"
         )
 
         parser.add_argument(
-            '--loop',dest="loop",default=False,
+            '--loop',
+            dest="loop",
+            default=False,
             type=bool,
             help="Repeat notification for the time set. Default [false]"
         )
--- Main.py
+++ Main.py
@@ -1,27 +1,34 @@
 from Cli import Cli
 from notifypy import Notify
 from pycoingecko import CoinGeckoAPI
-import locale,schedule,time,pprint
+import locale,schedule,time,pprint, re
 
 
 class Main(object):
-    def __init__(self,api,coin):
-        self.api = api
+    def __init__(self,coin):
+        self.api = CoinGeckoAPI()
         self.coin = coin
         self.currency = 'usd'
         self.crypto = self.api.get_price(ids= self.coin, vs_currencies=self.currency)
+
         #notification parameters
-        self.notify = Notify()
+        self.icon = "./images/bitcoin.png"
+        self.sound = "./sounds/gilfoyle_alert.wav"
+
         self.title = f"{self.coin.title()} Current Price"
         self.message = f"The Current Price of {self.coin.title()} is {self.coin_price()}"
-        self.notify.icon = icon
-        self.notify.audio = sound
-
+        
 
     def notify_me(self):
-        self.notify.title=self.title
-        self.notify.message = self.message
-        return self.notify.send()
+        notify = Notify(
+            default_notification_application_name="Crypto Alert",
+            default_notification_title=self.title, 
+            default_notification_message=self.message, 
+            default_notification_icon=self.icon, 
+            default_notification_audio=self.sound
+        )
+
+        return notify.send()
 
     def coin_price(self):
         coin_price = 0
@@ -39,35 +46,52 @@ class Main(object):
         currencies = self.api.get_supported_vs_currencies()
         return currencies
 
+    def getTimer(self, suffix):
+        suffix = suffix.lower()
+        type = 'minutes'
+        if suffix == 's':
+            type = 'seconds'
+        if suffix =='h':
+            type = 'hours'
+
+        return type
+
+    def startRunning(self, timer, on_loop=False):
+        formatted_timer = timer.strip().replace(" ", "")
+        digits = re.findall(r'\d+', formatted_timer)[0]
+        unit = re.findall(r'[a-zA-Z]+', formatted_timer)[0]
+        unit_method_name = self.getTimer(unit)
+        print(f"Running the notifier for {(self.coin).upper()} after every {digits} {unit_method_name}")
+
+        job = schedule.every(int(digits))
+        scheduling_method = getattr(job, unit_method_name)
+
+        scheduling_method.do(self.notify_me)
+        while on_loop:
+            schedule.run_pending()
+            time.sleep(1)
+        
+        self.notify_me()
 
 
-icon = "./images/bitcoin.png"
-sound = "./sounds/gilfoyle_alert.wav"
+
+
 locale.setlocale(locale.LC_ALL, '')
 
 
 if __name__ == '__main__':
 
     cli = Cli().parse_options()
-    api = CoinGeckoAPI()
-    app = Main(api,cli.coin)
-    pp = pprint.PrettyPrinter(indent=4,compact=True)
+    print(hasattr(cli, 'loop'))
+    # app = Main(cli.coin)
+    # pp = pprint.PrettyPrinter(indent=4,compact=True)
 
-    if cli.listcoins:
-        pp.pprint(app.list_coins())
-        exit()
-    if cli.listcurrencies:
-        pp.pprint(app.list_currencies())
-        exit()
+    # if cli.listcoins:
+    #     pp.pprint(app.list_coins())
+    #     exit()
+    # if cli.listcurrencies:
+    #     pp.pprint(app.list_currencies())
+    #     exit()
     
-    if cli.loop:
-        if cli.timeType[0].lower() == 's':
-            schedule.every(int(cli.time)).seconds.do(app.notify_me)
-        elif cli.timeType[0].lower() == 'm':
-            schedule.every(int(cli.time)).minutes.do(app.notify_me)
-        #loop for our schedules
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
-    app.notify_me()
+    # app.startRunning(cli.time, cli.loop)
 
