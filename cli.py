0c3de14d1af0457c3b9dea5270c22487fa9c9aff
musheabdulhakim <musheabdulhakim99@gmail.com>
2025-12-19 12:36:33 +0000

    code timer type is now attached to time

--- cli.py
+++ cli.py
@@ -4,28 +4,23 @@ parser = optparse.OptionParser()
 
     
 class Cli():
-    def __init__(self):
-       self.time = 5
-       self.currency = 'usd'
+   
+    def __init__(self,  time='1m', currency = 'usd', crypto= 'bitcoin'):
+       self.time = time
+       self.currency = currency
        self.getCurrencies = False
        self.getCoins = False
-       self.time_type = ''
-       self.crypto = ''
+       self.crypto = crypto
        self.argument_parser()
         
     def argument_parser(self):
         parser.add_option(
-            "-T",
+            "-t",
             "--time",
             dest="time",
-            help="Set timer for notifications."
+            help="Set timer for notifications. Example: 1m to set the timer to every 1 minute"
         )
-        parser.add_option(
-            "-t",
-            "--timetype",
-            dest="timeType",
-            help="Set timer type ('seconds' or 'minutes')."
-        )
+
         
         parser.add_option(
             '-c',
@@ -49,9 +44,10 @@ class Cli():
 
         (options,arguments) = parser.parse_args()
 
-        self.time = options.time
-        self.time_type = options.timeType
-        self.crypto = options.cryptoCoin
+        if options.time != None:
+            self.time = options.time
+        if options.cryptoCoin != None:
+            self.crypto = options.cryptoCoin
 
         get_coins = options.CoinsList
         get_currencies = options.Currencies
@@ -61,14 +57,7 @@ class Cli():
         if get_currencies != None:
             self.getCurrencies = get_currencies
 
-        if self.time_type != None:
-            if self.time_type.lower() == 'seconds' or self.time_type.lower() == 's':
-                self.time_type = 'seconds'
-            if self.time_type.lower() =='minutes' or self.time_type.lower() == 'm':
-                self.time_type = 'minutes'
-        
-
-        return self.time,self.time_type,self.crypto,self.getCurrencies,self.getCoins
+        return self.time,self.crypto,self.getCurrencies,self.getCoins
 
     
 
